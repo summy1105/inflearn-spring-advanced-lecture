e4a606605d639ae63e84d0e22040b529c2e3e9d1
summy1105 <dsm851105@gmail.com>
2024-02-15 00:01:25 +0900

    ThreadLocal

---
+++ src/test/java/hello/advanced/app/trace/threadlocal/code/FieldService.java
@@ -0,0 +1,25 @@
+package hello.advanced.app.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FieldService {
+
+    private String nameStore;
+
+    public String logic(String name) {
+        log.info("save name={} -> nameStore={}", name, nameStore);
+        nameStore = name;
+        sleep(1000);
+        log.info("retrieve nameStore={}", nameStore);
+        return nameStore;
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
